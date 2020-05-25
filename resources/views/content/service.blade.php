3edfb9c0b0754ea1e877a7880b30aa4f391d4ad3
setiaraf14 <setia.rafif14@gmail.com>
2020-05-25 14:53:42 +0700

    penambahan untuk halaman home dan service

--- resources/views/content/service.blade.php
+++ resources/views/content/service.blade.php
@@ -6,8 +6,227 @@
 
 {{-- Content --}}
 
-<h1>ini halaman service</h1>
 
+<section class="service-1">
+    <div class="container-fluid">
+      <div class="row text-center">
+        <div class="col-lg-12 col-md-12 col-12">
+          <h1>SERVICE.</h1>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+            <div class="col-lg-4 align-items-center">
+                <div class="service-1-icon-box p-2 text-center pt-2">
+                    <div class="service-1-icon"><i class="fas fa-concierge-bell fa-4x"></i></i></i></div>
+                    <br>
+                    <h4><a href="">24 Hour ON</a></h4>
+                    <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                </div>
+            </div>
+            <div class="col-lg-4 align-items-center">
+                <div class="service-1-icon-box p-2 text-center pt-2">
+                    <div class="service-1-icon"><i class="fas fa-passport fa-4x"></i></i></div>
+                    <br>
+                    <h4><a href="">In Every Country</a></h4>
+                    <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                </div>
+            </div>
+            <div class="col-lg-4 align-items-center">
+                <div class="service-1-icon-box p-2 text-center pt-2">
+                    <div class="service-1-icon"><i class="fas fa-tachometer-alt fa-4x"></i></i></i></div>
+                    <br>
+                    <h4><a href="">Fast Service</a></h4>
+                    <p>Voluptatum deleniti atque corrupti quos dolores et quas excepturi</p>
+                </div>
+            </div>
+      </div>
+      <br><br><br>
+      <div class="row">
+            <div class="col-lg-4 align-items-center">
+                <div class="service-1-icon-box p-2 text-center pt-2">
+                    <div class="service-1-icon"><i class="fas fa-edit fa-4x"></i></div>
+                    <br>
+                    <h4><a href="">Good Standard of Service</a></h4>
+                    <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                </div>
+            </div>
+            <div class="col-lg-4 align-items-center">
+                <div class="service-1-icon-box p-2 text-center pt-2">
+                    <div class="service-1-icon"><i class="fas fa-briefcase fa-4x"></i></div>
+                    <br>
+                    <h4><a href="">Maintanance</a></h4>
+                    <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                </div>
+            </div>
+            <div class="col-lg-4 align-items-center">
+                <div class="service-1-icon-box p-2 text-center pt-2">
+                    <div class="service-1-icon"><i class="fas fa-business-time fa-4x"></i></div>
+                    <br>
+                    <h4><a href="">Service Waranty</a></h4>
+                    <p>Voluptatum deleniti atque corrupti quos dolores et quas excepturi</p>
+                </div>
+            </div>
+      </div>
+      <br><br>
+        <div class="row service-1-search p-10 pt-10" style="background-image: url('img/service-cube.jpg');">
+            <div class="col-md-2"></div>
+            <div class="col-md-4 col-sm-12">
+                <h1>Suscribe Our Newsletter</h1>
+                <h3>Suscribe our newsletter to get offers and about new Places To Discover</h3>
+            </div>
+            <div class="col-md-4 col-sm-12" style="padding-left: 2px; padding-top: 30px; padding-bottom: 30px;">
+                <input type="text" class="form-control" placeholder="Your Email" aria-label="Recipient's username" aria-describedby="basic-addon2"style="background-color:white;">
+            </div>
+            <div class="col-md-2" style="padding-left: 2px; padding-top: 30px; padding-bottom: 30px;">
+                <button type="button" class="btn btn-danger">Search</button>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+  </section>
+
+  <section class="service-1">
+    <div class="container-fluid">
+      <div class="row text-center">
+        <div class="col-lg-12 col-md-12 col-12">
+          <h1>Pricing.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto pariatur eius officiis ratione quibusdam, quos itaque facere sequi? Saepe consequatur incidunt quisquam iste temporibus quos, non distinctio ducimus eveniet blanditiis natus itaque nisi libero tempora harum.</p>
+        </div>
+      </div>
+      <br><br>
+      <div class="row">
+            <div class="col-lg-4 d-flex justify-content-end">
+                <div class="service-1-pricing p-3">
+                    <h2>Free Plan</h2>
+                    <h1>$0</h1>
+                    <ul>
+                        <li><i class="fas fa-chess-queen fa-2x"></i> Ullamco laboris nisi ut aliquip</li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i> Nec feugiat nisl pretium</li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i> Nulla at volutpat diam uteera</li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i><s>Pharetra massa massa ultricies</s></li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i><s>Massa ultricies mi quis hendrerit</s> </li>
+                        <br>
+                    </ul>
+                    <div class="service-1-pricing-choice">
+                        <h5>Get Started !</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 d-flex justify-content-center">
+                <div class="service-1-pricing p-3">
+                    <h2>Free Plan</h2>
+                    <h1>$25</h1>
+                    <ul>
+                        <li><i class="fas fa-chess-queen fa-2x"></i> Ullamco laboris nisi ut aliquip</li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i> Nec feugiat nisl pretium</li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i> Nulla at volutpat diam uteera</li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i>Pharetra massa massa ultricies</li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i><s>Massa ultricies mi quis hendrerit</s> </li>
+                        <br>
+                    </ul>
+                    <div class="service-1-pricing-choice">
+                        <h5>Get Started !</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 d-flex justify-content-start">
+                <div class="service-1-pricing p-3">
+                    <h2>Free Plan</h2>
+                    <h1>$100</h1>
+                    <ul>
+                        <li><i class="fas fa-chess-queen fa-2x"></i> Ullamco laboris nisi ut aliquip</li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i> Nec feugiat nisl pretium</li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i> Nulla at volutpat diam uteera</li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i>Pharetra massa massa ultricies</li>
+                        <br>
+                        <li><i class="fas fa-chess-queen fa-2x"></i>Massa ultricies mi quis hendrerit</li>
+                        <br>
+                    </ul>
+                    <div class="service-1-pricing-choice">
+                        <h5>Get Started !</h5>
+                    </div>
+                </div>
+            </div>
+      </div>
+    </div>
+    <br><br><br>
+  </section>
+
+  <section class="service-testimoni" style="background-image: url('img/service-black.jpg'); background-repaet">
+    <div class="container-fluid" >
+        <div class="row text-center">
+                <div class="col-lg-12 col-md-12 col-12">
+                  <h1>TESTIMONI.</h1>
+                </div>
+        </div>
+        <br><br>
+        <div class="row service-testimoni-carosel" >
+            
+            <div class="col-lg-2"></div>
+            <div id="demo" class="carousel slide col-md-8 mt-3" data-ride="carousel">
+
+                <ul class="carousel-indicators">
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+                </ul>
+                <!-- Ganti gambar dengan file gambar kalian -->
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="service-member d-flex justify-content-center">
+                            <img src="{{ asset('img/home-rafif2.jpg') }}" alt="" class="rounded-circle" width="100" height="100">
+                            <div class="service-caption">
+                                <h2>Liverpool FC</h2>
+                                <br>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident culpa beatae explicabo amet minima sed facilis ea aliquam, ad necessitatibus unde ipsum. Itaque, nihil. Quia nihil aliquid possimus eaque facilis.</p>
+                            </div>
+                        </div>   
+                    </div>
+                    <div class="carousel-item">
+                        <div class="service-member d-flex justify-content-center">
+                            <img src="{{ asset('img/home-rangga.jpg') }}" alt="" class="rounded-circle" width="100" height="100">
+                            <div class="service-caption">
+                                <h2>Master Rangga</h2>
+                                <br>
+                                <p>Amet consectetur adipisicing elit. Provident culpa beatae explicabo amet minima sed facilis ea aliquam, ad necessitatibus unde ipsum. Itaque, nihil. Quia nihil aliquid possimus eaque facilis.</p>
+                            </div>
+                        </div>   
+                    </div>
+                <div class="carousel-item">
+                    <div class="service-member d-flex justify-content-center">
+                        <img src="{{ asset('img/home-lukman.jpg') }}" alt="" class="rounded-circle" width="100" height="100">
+                        <div class="service-caption">
+                            <h2>Master Lukman</h2>
+                            <br>
+                            <p>Provident culpa beatae explicabo amet minima sed facilis ea aliquam, ad necessitatibus unde ipsum. Itaque, nihil. Quia nihil aliquid possimus eaque facilis.</p>
+                        </div>
+                    </div>   
+                </div>
+                </div>
+                
+                <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+                </a>
+                <a class="carousel-control-next" href="#demo" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+                </a>
+            </div>  
+        </div>
+    </div>
+  </section>
+     
 {{-- endContent --}}
 
 @endsection
